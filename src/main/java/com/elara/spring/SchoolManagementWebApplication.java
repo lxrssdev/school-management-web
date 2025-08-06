db2f74b1fece90d2db911c622d261d086f05f394
Jaydeeeveloper <manelitos123@gmail.com>
2025-08-06 01:23:47 -0600

    Primer commit

---
+++ src/main/java/com/elara/spring/SchoolManagementWebApplication.java
@@ -0,0 +1,13 @@
+package com.elara.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementWebApplication.class, args);
+	}
+
+}
