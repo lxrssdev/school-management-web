2c4e4e5630dd20b2a6e79823ba4b68a81563e132
Jaydeeeveloper <manelitos123@gmail.com>
2025-08-07 02:26:28 -0600

    navbar y agregar maestro

--- src/main/java/com/elara/spring/controllers/TeacherController.java
+++ src/main/java/com/elara/spring/controllers/TeacherController.java
@@ -1,12 +1,15 @@
 package com.elara.spring.controllers;
 
+import com.elara.spring.entities.Teacher;
 import com.elara.spring.services.TeacherService;
 import jakarta.servlet.ServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/dashboard")
@@ -24,6 +27,28 @@ public class TeacherController {
         model.addAttribute("title", "Lista de maestros");
         model.addAttribute("teachers", teacherService.listTeachers());
         return "teachers";
+    }
+
+    @GetMapping("/form-create")
+    public String formCreate(Model model){
+        model.addAttribute("title", "Formulario | Creación de maestros!");
+        model.addAttribute("teacher", new Teacher());
+        return "form";
+    }
+
+    @PostMapping("/dashboard/teachers")
+    public String processFormCreate(@Valid Teacher teacher, BindingResult result, Model model, RedirectAttributes redirect, SessionStatus status) {
+        if(result.hasErrors()) {
+            model.addAttribute("title", "Formulario en validación | Completa los campos");
+            return "form";
+        }
+        String message = (teacher.getId() != null && teacher.getId() > 0)
+                ? "Maestro "+ teacher.getFullName() + " actualizado correctamente"
+                : "Maestro " + teacher.getFullName() + " creado correctamente";
+        teacherService.save(teacher);
+        status.setComplete();
+        redirect.addFlashAttribute("success", message);
+        return "redirect:/dashboard/teachers";
     }
 
 
