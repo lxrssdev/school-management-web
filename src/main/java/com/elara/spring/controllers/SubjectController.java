0620508eb74044562c764b9a81874d454341264e
Jaydeeeveloper <manelitos123@gmail.com>
2025-08-07 23:49:15 -0600

    listado de materias

--- src/main/java/com/elara/spring/controllers/SubjectController.java
+++ src/main/java/com/elara/spring/controllers/SubjectController.java
@@ -1,4 +1,25 @@
 package com.elara.spring.controllers;
 
+import com.elara.spring.services.SubjectService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
 public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    public SubjectController(SubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    @GetMapping("/subjects")
+    public String listSubjects(Model model) {
+        model.addAttribute("title", "Listado de materias");
+        model.addAttribute("subjects", subjectService.getAllSubjects());
+        return "subjects";
+    }
 }
